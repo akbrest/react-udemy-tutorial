635e3bdfcb410be6216db7f8880e70dee36165ea
Aliaksandr Kandratsiuk <ak.brest@gmail.com>
2023-11-12 23:14:06 +0200

    Added errorSlice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './sclices/booksSlice';
 import filterReducer from './sclices/filterSlice';
+import errorReducer from './sclices/errorSlice';
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
     filter: filterReducer,
+    error: errorReducer,
   },
 });
 
