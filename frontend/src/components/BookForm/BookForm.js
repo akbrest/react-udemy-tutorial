3c5f9662d1e0b23e23044d5c17e547dd42be92c3
Aliaksandr Kandratsiuk <ak.brest@gmail.com>
2023-11-13 11:03:14 +0200

    Dispatch error in the async thunk

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -31,7 +31,7 @@ const BookForm = () => {
   };
 
   const handleAddRandomViaAPI = () => {
-    dispatch(fetchBook());
+    dispatch(fetchBook('http://localhost:4000/random-book'));
   };
 
   return (
