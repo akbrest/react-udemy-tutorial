ff5181ec3c3a000f7d39b174fc952f544a15fa49
Aliaksandr Kandratsiuk <ak.brest@gmail.com>
2023-11-14 23:03:53 +0200

    Another option for extraReducers

--- frontend/src/redux/sclices/booksSlice.js
+++ frontend/src/redux/sclices/booksSlice.js
@@ -46,6 +46,14 @@ const booksSlice = createSlice({
       // );
     },
   },
+  // Option 1
+  // extraReducers: {
+  //   [fetchBook.fulfilled]: (state, action) => {
+  //     if (action.payload.title && action.payload.author)
+  //       state.push(createBookWithId(action.payload, 'API'));
+  //   },
+  // },
+  // Option 2
   extraReducers: (builder) => {
     builder.addCase(fetchBook.fulfilled, (state, action) => {
       if (action.payload.title && action.payload.author)
